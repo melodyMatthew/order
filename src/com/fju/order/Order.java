fc271a471abc51947f05430a46857c3e1999efc9
melodyMatthew <meloody,s96309@gmail.com>
2022-04-29 16:09:26 +0800

    j

--- src/com/fju/order/Order.java
+++ src/com/fju/order/Order.java
@@ -3,15 +3,16 @@ package com.fju.order;
 public class Order {
     int amount;
     Delivery delivery;
-    int shipFeed;
 
 
-    public Order(int amount, Delivery delivery,int shipFeed) {
+
+    public Order(int amount, Delivery delivery) {
         this.amount = amount;
         this.delivery = delivery;
-        this.shipFeed =
 
     }
+
+
 
 
 }
